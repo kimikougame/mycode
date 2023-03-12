01966be7369cdfa70a90f507f873127760610d29
kimikougame <127109122+kimikougame@users.noreply.github.com>
2023-03-12 16:13:05 +0900

    2023_03_12_16h12m_upload

---
+++ server/dictionary/response_json.py
@@ -0,0 +1,24 @@
+#クライアントからのGetメソッドに対して値を返す
+from django.http import JsonResponse
+#def testView(request):
+
+  #[モデルのquerysetオブジェクトから指定したプロパティの値を取り出しリストにする。]
+
+  #test = [モデルオブジェクト].objects.all().values_list('pk',flat=True)
+  #number = list(test)
+
+  #[モデルのquerysetオブジェクトから指定したプロパティの値を取り出しリストにする。]
+  
+  #[シリアライズのためjavascriptのオブジェクト記述をする。]
+
+  #name = {
+    #"pokemon":number
+  #}
+
+  #[シリアライズのためjavascriptのオブジェクト記述をする。]
+
+  #[シリアライズしてクライアントにレスポンスする。]
+
+  #return JsonResponse(name,safe=False);
+
+  #[シリアライズしてクライアントにレスポンスする。]
